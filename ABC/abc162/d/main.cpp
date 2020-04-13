d9ef2243d341bd7df33b830b4d3ffa78c5dd6c64
Hiroki Fujioka <hiroki976@gmail.com>
2020-04-13 09:01:07 +0900

    fix abc162 d

--- ABC/abc162/d/main.cpp
+++ ABC/abc162/d/main.cpp
@@ -11,8 +11,8 @@ int main() {
     ll ng = 0, r = 0, g = 0, b = 0;
     rep(i, n) {
         s[i] == 'R' ? r++ : s[i] == 'G' ? g++ : b++;
-        rep(d, n) {
-            int j = i + d, k = j + d;
+        for (int j = i + 1; j < n - 1; j++) {
+            int k = 2 * j - i;
             if (k >= n) break;
             if (s[i] != s[j] && s[i] != s[k] && s[j] != s[k]) ng++;
         }
