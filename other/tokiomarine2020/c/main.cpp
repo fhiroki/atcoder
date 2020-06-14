6f70beaedf26c27ff581cba27d3b05af104149fd
Hiroki Fujioka <hiroki976@gmail.com>
2020-06-14 09:43:33 +0900

    solve tokiomarine2020 a,b,c

--- other/tokiomarine2020/c/main.cpp
+++ other/tokiomarine2020/c/main.cpp
@@ -6,8 +6,30 @@ using namespace std;
 int main() {
     int n, k;
     cin >> n >> k;
-    int a[n];
+    vector<int> a(n, 0);
     rep(i, n) cin >> a[i];
+
+    rep(j, k) {
+        vector<int> b(n, 0);
+        rep(i, n) {
+            int l = max(0, i - a[i]);
+            int r = min(n - 1, i + a[i]);
+            b[l]++;
+            if (r + 1 < n) b[r + 1]--;
+        }
+        for (int i = 1; i < n; i++) b[i] += b[i - 1];
+
+        if (a == b) break;
+        a = b;
+    }
+
+    rep(i, n) {
+        cout << a[i];
+        if (i == n - 1)
+            cout << endl;
+        else
+            cout << ' ';
+    }
 
     return 0;
 }
