47e4f642c39efe7c608eb3297070e6ff38ac1593
Hiroki Fujioka <hiroki976@gmail.com>
2020-06-21 15:44:11 +0900

    solve 069 a,b,c,d

---
+++ ABC/abc069/a/main.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < n; i++)
+typedef long long ll;
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    int ans = (n - 1) * (m - 1);
+
+    cout << ans << endl;
+    return 0;
+}
