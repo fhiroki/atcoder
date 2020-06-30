3114774e52187443da91d7df0dfa359e52e911db
Hiroki Fujioka <hiroki976@gmail.com>
2020-06-30 19:52:05 +0900

    solve 057 a,b,c

--- ABC/abc057/c/main.cpp
+++ ABC/abc057/c/main.cpp
@@ -2,21 +2,16 @@
 using namespace std;
 using ll = long long;
 
-int cnt_digits(ll N) { return to_string(N).length(); }
-
 int main() {
-  ll N;
-  cin >> N;
+    ll n;
+    cin >> n;
 
-  int ans = cnt_digits(N);
-  for (ll A = 1LL; A * A <= N; A++) {
-    if (N % A != 0) continue;
-    const ll B = N / A;
-    const int cur = max(cnt_digits(A), cnt_digits(B));
-    if (ans > cur) ans = cur;
-  }
+    size_t ans = 1e9;
+    for (ll a = 1; a * a <= n; a++) {
+        if (n % a != 0) continue;
+        ans = min(ans, max(to_string(a).length(), to_string(n / a).length()));
+    }
 
-  cout << ans << endl;
-
-  return 0;
+    cout << ans << endl;
+    return 0;
 }
