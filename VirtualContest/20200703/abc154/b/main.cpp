e823f54b8710a5c8f241a3fab1ede970c8aaa56e
Hiroki Fujioka <hiroki976@gmail.com>
2020-07-03 09:03:59 +0900

    solve asakatsu0703

---
+++ VirtualContest/20200703/abc154/b/main.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < n; i++)
+typedef long long ll;
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+
+    rep(i, s.size()) cout << 'x';
+    cout << endl;
+
+    return 0;
+}
