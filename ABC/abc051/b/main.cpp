037157ff7c2098b2024a0a9fe6f206ca8cc042b5
Hiroki Fujioka <hiroki976@gmail.com>
2020-07-10 10:00:04 +0900

    solve 051 a,b,c

--- ABC/abc051/b/main.cpp
+++ ABC/abc051/b/main.cpp
@@ -4,14 +4,14 @@ using namespace std;
 using ll = long long;
 
 int main() {
-    int K, S;
-    cin >> K >> S;
+    int k, s;
+    cin >> k >> s;
 
     int ans = 0;
-    for (int x = 0; x <= K; x++) {
-        for (int y = 0; y <= K; y++) {
-            int z = S - (x + y);
-            if (z >= 0 && z <= K) ans++;
+    for (int x = 0; x <= k; x++) {
+        for (int y = 0; y <= k; y++) {
+            int z = s - (x + y);
+            if (z >= 0 && z <= k) ans++;
         }
     }
 
