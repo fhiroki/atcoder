765f654f38407e88cb10992d0690b4627e422dda
Hiroki Fujioka <hiroki976@gmail.com>
2020-07-26 07:37:07 +0900

    solve 013 a,b

---
+++ ABC/abc013/b/main.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < n; i++)
+typedef long long ll;
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+
+    int ans = min(abs(a - b), 10 - abs((a - b)));
+
+    cout << ans << endl;
+    return 0;
+}
