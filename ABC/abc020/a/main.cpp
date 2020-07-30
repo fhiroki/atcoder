5ec1d6373cb95572824d12315756fb813c8100d9
Hiroki Fujioka <hiroki976@gmail.com>
2020-07-31 08:49:45 +0900

    solve 020 a,b

---
+++ ABC/abc020/a/main.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < n; i++)
+typedef long long ll;
+using namespace std;
+
+int main() {
+    int q;
+    cin >> q;
+
+    if (q == 1)
+        cout << "ABC" << endl;
+    else
+        cout << "chokudai" << endl;
+    return 0;
+}
