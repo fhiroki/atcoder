ae459965a17a7cef1f2da79cb1773f7d5d558376
Hiroki Fujioka <hiroki976@gmail.com>
2020-08-11 08:19:05 +0900

    solve 034 a,b

---
+++ ABC/abc034/a/main.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < n; i++)
+typedef long long ll;
+using namespace std;
+
+int main() {
+    int x, y;
+    cin >> x >> y;
+
+    if (y > x)
+        cout << "Better" << endl;
+    else
+        cout << "Worse" << endl;
+    return 0;
+}
