15b08570ddea673086538f84a1538138b0e1aea6
Hiroki Fujioka <hiroki976@gmail.com>
2020-08-18 09:03:21 +0900

    solve 139 d

---
+++ ABC/abc139/d/main.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < n; i++)
+typedef long long ll;
+using namespace std;
+
+int main() {
+    ll n;
+    cin >> n;
+
+    ll ans = n * (n - 1) / 2;
+
+    cout << ans << endl;
+    return 0;
+}
