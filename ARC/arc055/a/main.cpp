73822fbb82c3850e676cf7130baa3d2e71a5fbf0
Hiroki Fujioka <hiroki976@gmail.com>
2020-08-28 23:24:15 +0900

    solve arc055 a

---
+++ ARC/arc055/a/main.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < n; i++)
+typedef long long ll;
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    cout << 1;
+    rep(i, n - 1) cout << 0;
+    cout << 7 << endl;
+
+    return 0;
+}
