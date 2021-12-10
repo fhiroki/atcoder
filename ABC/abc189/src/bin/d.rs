e0a68a7952d5fbd39d08de5801db30603470acbf
fhiroki <hiroki976@gmail.com>
2021-12-10 09:19:07 +0900

    solve abc189 d

--- ABC/abc189/src/bin/d.rs
+++ ABC/abc189/src/bin/d.rs
@@ -1,6 +1,18 @@
-use proconio::{input, fastout};
+use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        mut s: [String; n]
+    }
+
+    let mut res: u64 = 1;
+    for i in 0..n {
+        if s[i] == "OR" {
+            res += 2u64.pow(i as u32 + 1);
+        }
+    }
+
+    println!("{}", res);
 }
